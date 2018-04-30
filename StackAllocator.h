a49a05e16b4a81bd13b9da323c2495a54b41527b
AminiL <aleshka1999@yandex.ru>
2018-04-30 22:43:26 +0400

    first commit

---
+++ StackAllocator.h
@@ -0,0 +1,98 @@
+#pragma once
+#include <iterator>
+#include <memory>
+class List {
+private:
+	size_t len_;
+	List *prev_;
+	char *start_;
+public:
+	explicit List(size_t len, List *prev = nullptr) :
+		len_(len),
+		prev_(prev),
+		start_(new char[len])
+	{}
+	~List() {
+		delete[] start_;
+		if (prev_ != nullptr)
+			delete prev_;
+	}
+	char* get_start() {
+		return start_;
+	}
+};
+
+class buffer {
+private:
+	List* now_;
+	size_t len_;
+	size_t cnt_of_busies_memes_;
+public:
+	explicit buffer(size_t len) :
+		len_(len),
+		now_(new List(len)),
+		cnt_of_busies_memes_(0)
+	{}
+
+	void upd_list() {
+		now_ = new List(len_, now_);
+		cnt_of_busies_memes_ = 0;
+	}
+	char* get_free_pos() {
+		return now_->get_start() + cnt_of_busies_memes_;
+	}
+	char* allocate(size_t n, size_t T_size) {
+		size_t cnt_of_memes = n * T_size;
+		if (cnt_of_busies_memes_ + cnt_of_memes > len_) {
+			upd_list();
+		}
+		cnt_of_busies_memes_ += cnt_of_memes;
+		return get_free_pos();
+	}
+	~buffer() {
+		if (now_)
+			delete now_;
+	}
+};
+
+template < typename T >
+class StackAllocator {
+public:
+	typedef T value_type;
+	typedef T* pointer;
+	typedef const T* const_pointer;
+	typedef T& reference;
+	typedef const T& const_reference;
+	typedef std::size_t size_type;
+	typedef std::ptrdiff_t difference_type;
+	template< class U >
+	struct rebind { 
+		typedef StackAllocator<U> other; 
+	};
+	template <class U>
+	friend class StackAllocator;
+	template <class U>
+	friend class StackAllocator;
+private:
+	size_t len_;
+	std::shared_ptr <buffer> buf_;
+public:
+	explicit StackAllocator(size_t len = 1e7) :
+		len_(len),
+		buf_(new buffer(len))
+	{}
+	template < typename U >
+	StackAllocator(const StackAllocator < U >& other) {
+		buf_ = other.buf_;
+		len_ = other.len_;
+	}
+	T* allocate(size_t n) {
+		return reinterpret_cast <T*> (buf_->allocate(n, sizeof(T)));
+	}
+
+	void deallocate(T* p, size_t n) {
+		return;
+	}
+
+	~StackAllocator() {	}
+};
